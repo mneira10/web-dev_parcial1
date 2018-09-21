396c1c4fcf99717c748061a68086ba3726833ecf
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-21 15:55:52 -0500

    csv loads, visualiation works

--- parcial1/front/src/App.js
+++ parcial1/front/src/App.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import vegaEmbed from 'vega-embed';
+import Papa from 'papaparse';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state= {
-      spec : {
-        "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-        "description": "A simple bar chart with embedded data.",
-        "data": {
-          "name": "myData" 
-        },
-        "mark": "bar",
-        "encoding": {
-          "y": {"field": "a", "type": "ordinal"},
-          "x": {"field": "b", "type": "quantitative"}
-        }
-      },
-      myData : [
-        {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
-        {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
-        {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
-      ]
+      spec : this.defaultSpec,
+      data : []
     }
+
+    this.setData = this.setData.bind(this);
+  }
+
+  defaultSpec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+    "description": "A simple bar chart with embedded data.",
+    "data": {
+      "name": "myData" 
+    },
+    "mark": "bar",
+    "encoding": {
+      "y": {"field": "a", "type": "ordinal"},
+      "x": {"field": "b", "type": "quantitative"}
+    }
+  };
+
+  setData(results){
+    const data = results.data;
+    this.setState({data:data});
   }
 
   
@@ -35,19 +41,52 @@ class App extends Component {
       <h1>Make your own vega visualizations!</h1>
 
        
-        <textarea  onChange={(event)=>{this.setState({spec:event.target.value})}}name="inputJSON" id="inputJSON" cols="30" rows="10"></textarea>
+        <textarea  onChange={
+          (event)=>{
+
+              this.setState({spec:event.target.value});
+          
+        } } name="inputJSON" id="inputJSON" cols="100" rows="20" >
+        
+        </textarea>
         <br/>
-        <input type="file" onChange={(event)=> console.log(event.target.value)}/>
+        <input type="file" onChange={(event)=> {
+
+            const data = Papa.parse(event.target.files[0], {
+              header: true,
+              dynamicTyping: true,
+              delimiter : ",",
+              complete: this.setData
+
+            });
+          }
+        }/>
         <br/>
 
        <button onClick = {()=>{
+        console.log(this.state.data[0]);
+        console.log(this.state.data);
+        const embed_opt = {"mode": "vega-lite"};           
+        let specJSON = null;
 
-         const embed_opt = {"mode": "vega-lite"};    
-         const el = this.viz;
-         const view = vegaEmbed(this.viz, JSON.parse(this.state.spec), embed_opt)
-         .catch(error => console.log(el, error))
-         .then((res) =>  res.view.insert("myData", this.state.myData).run());
-         
+        try{
+          specJSON = JSON.parse(this.state.spec);
+        }
+        catch(error){
+          alert("Invalid JSON format")
+        }
+
+        if(specJSON!==null){
+          vegaEmbed(this.viz, specJSON, embed_opt)
+          .then((res) =>  res.view.insert("data", this.state.data).run())
+          .catch(error=>{
+            console.log(error);
+            alert("Invalid JSON format for vega-lite spec");
+
+          });
+        }
+
+        
         }}>Render Viz!</button>
 
         <br/>
